367484bbc2ba378feba5187d2d0f14a09fb8df44
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-22 16:41:13 +0200

    Update 1.js

--- Tasks/01/1.js
+++ Tasks/01/1.js
@@ -1,3 +1,14 @@
+/*
+1. Even Position Element
+Write a function that finds the elements at even positions in an array.
+The input comes as an array of string elements.
+The output is printed on the console. Collect all elements in a string, separated by space.
+
+EXAMPLE
+Param 1: ['20', '30', '40', '50', '60']
+Result: [20, 40, 60]
+*/
+
 function solve(input) {
 	let outputArr = [];
 
